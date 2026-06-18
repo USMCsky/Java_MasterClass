16eb37eb931d0f020557ea123e10f38d23f683df
USMCsky <57950108+USMCsky@users.noreply.github.com>
2026-06-18 16:08:07 -0500

    update Main.java

---
+++ src/com/usmcsky/test/Main.java
@@ -0,0 +1,32 @@
+package com.usmcsky.test;
+
+public class Main {
+
+    static void main(String[] args) {
+        String question = "What is your name?";
+        String response = IO.readln(question);
+        IO.println(response);
+
+        System.out.println("Hello World!");
+
+        //Variables - primitive datatypes
+        int number = 5;
+        double decNumber = number;
+        boolean  bool = true;
+        char letter = 'A';
+
+        IO.println(number);
+        IO.println(decNumber);
+        IO.println(bool);
+        IO.println(letter);
+
+        //Variables non-primitive types (wrappers)
+        Integer number2 = 1;
+        Double double2 = 2.0;
+        Boolean bool2 = false;
+        Character char22 = 'D';
+
+
+
+    }
+}
